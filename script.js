8b52bedb529af88ab1fe308e357108d3e153c249
vicxtr <joao.silva4011@aluno.ce.gov.br>
2025-08-12 12:56:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const Nome = "Jorge"
+let idade = 51
+let cidade = "caninde"
+let profissao = "carteiro"
+let anoatual = 2025
+let anonascimento = anoatual-idade
+let salario = 11330
+let Bonus = 6090
+let TotalMensal = 17420
+let TotalAno = 209040
+let TotalAnualdobrado = 209040 * 2
+alert(`===== RELATORIO DO USUARIO =====
+nome: ${Nome}
+idade: ${idade}
+Ano de Nascimento: ${anonascimento}
+cidade: ${cidade}
+profissao: ${profissao}
+
+salario base: ${salario}
+bonus: ${Bonus}
+total mensal: ${TotalMensal}
+total ano: ${TotalAno}
+total anual (salario dobrado): ${TotalAnualdobrado}
+============================`)
